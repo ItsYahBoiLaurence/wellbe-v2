528a831438d86b2f1218cfda5b136be5e63ab32e
ItsYahBoiLaurence <laurence@mayan.com.ph>
2025-01-17 18:09:52 +0800

    Profile Flow Finished

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,21 @@ import { theme } from './theme';
 import queryClient from './queryClient';
 import Routes from './Routes';
 import { Authentication } from './contexts/Authentication'
+import { ModalsProvider } from '@mantine/modals';
 
 function App() {
   return (
     <BrowserRouter>
       <MantineProvider theme={theme} stylesTransform={emotionTransform}>
+
         <MantineEmotionProvider>
           <QueryClientProvider client={queryClient}>
             {/* <AuthProvider> */}
             {/* <ProfileProvider> */}
             <Authentication>
-              <Routes />
+              <ModalsProvider>
+                <Routes />
+              </ModalsProvider>
             </Authentication>
             {/* </ProfileProvider> */}
             {/* </AuthProvider> */}
--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,20 +1,112 @@
-import { Container, Avatar, Text } from '@mantine/core';
+import { Container, Avatar, Text, Box, Stack, Paper, Flex, Modal, Button } from '@mantine/core';
+import { useContext, useEffect, useState } from 'react';
+import { AuthenticationContext } from '../../contexts/Authentication';
+import api from '../../api/api';
+import { IconChevronRight } from '@tabler/icons-react';
+import { useDisclosure } from '@mantine/hooks';
+import { modals } from "@mantine/modals"
+
 
 const ProfilePage = () => {
+
+  const { user, logout } = useContext(AuthenticationContext)
+  const [opened, { open, close }] = useDisclosure(false);
+
+  const [userProfile, setUserProfile] = useState({})
+  const [userImage, setUserImage] = useState("")
+
+  const fetchUserProfile = async (email: string | undefined, company: string) => {
+    try {
+      const params = {
+        email: email,
+        company: company
+      }
+      await api.get('/api/employee/profile', { params }).then((response) => {
+        console.log(response.data)
+        setUserProfile(response.data)
+      })
+    } catch (error) {
+      throw error
+    }
+  }
+  const openModal = () =>
+    modals.openConfirmModal({
+      title: 'Logout Confirmation',
+      centered: true,
+      children: (
+        <Text size="md">
+          Are you sure you want to logout?
+        </Text>
+      ),
+      labels: { confirm: 'Confirm', cancel: "Cancel" },
+      confirmProps: { color: 'red' },
+      onCancel: () => console.log('Cancel'),
+      onConfirm: () => logout(),
+    });
+  useEffect(() => { fetchUserProfile(user?.email as string, "Sample Company") }, [])
+
+  useEffect(() => {
+    if (userProfile.FirstName) {
+      setUserImage(userProfile.FirstName)
+    }
+    return;
+  }, [userProfile])
   return (
-    <Container >
-      <Avatar
-        src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-8.png"
-        size={120}
-        radius={120}
-        mx="auto"
-      />
-      <Text ta="center" fz="lg" fw={500} mt="md">
-        Cici
-      </Text>
-      <Text ta="center" c="dimmed" fz="sm">
-        jfingerlicker@me.io • Art director
-      </Text>
+    <Container py={24}>
+      <Modal opened={opened} onClose={close} centered>
+        <Text style={{ textAlign: 'center' }}>Are you sure you want to Logout?</Text>
+        <Button variant="filled" size="xs">Button</Button>
+      </Modal>
+      <Box>
+        <Avatar
+          size={80}
+          radius={120}
+          mx="auto"
+        >
+          {userImage[0]}
+        </Avatar>
+        <Text ta="center" fz="lg" fw={500} mt="md">
+          User Profile
+        </Text>
+      </Box>
+      <Stack
+        h={450}
+        w={'90vw'}
+        bg="var(--mantine-color-body)"
+        align="stretch"
+        justify="center"
+      >
+        <Paper withBorder p="md">
+          <Flex justify={'space-between'}>
+            <Text>First name</Text>
+            <Text>{userProfile.FirstName}</Text>
+          </Flex>
+        </Paper>
+        <Paper shadow="xs" withBorder p="md">
+          <Flex justify={'space-between'}>
+            <Text>Last name</Text>
+            <Text>{userProfile.LastName}</Text>
+          </Flex>
+        </Paper>
+        <Paper shadow="xs" withBorder p="md">
+          <Flex justify={'space-between'}>
+            <Text>Email</Text>
+            <Text>{userProfile.email}</Text>
+          </Flex>
+        </Paper>
+        <Paper shadow="xs" withBorder p="md">
+          <Flex justify={'space-between'}>
+            <Text>Company</Text>
+            <Text>{userProfile.company}</Text>
+          </Flex>
+        </Paper>
+        <Paper shadow="xs" withBorder p="md" onClick={openModal} style={{ cursor: 'pointer' }}>
+          <Flex justify={'space-between'} align={'center'}>
+            <Text>Logout</Text>
+            <Text style={{ paddingTop: 5 }}><IconChevronRight /></Text>
+          </Flex>
+        </Paper>
+      </Stack>
     </Container>
   );
 };
