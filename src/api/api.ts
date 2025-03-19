3d1ab577db4c7ef9f93c793de2acbb4c75bdedac
ItsYahBoiLaurence <laurence@mayan.com.ph>
2025-03-19 12:09:25 +0800

    CLIENT_TOKEN added per request

--- src/api/api.ts
+++ src/api/api.ts
@@ -11,7 +11,7 @@ const api = axios.create({
 
 api.interceptors.request.use(
     (config) => {
-        const token = localStorage.getItem("TOKEN");
+        const token = localStorage.getItem("CLIENT_TOKEN");
         if (token) {
             config.headers.Authorization = `Bearer ${token}`
         }
